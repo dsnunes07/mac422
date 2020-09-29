ca85f5c8b8ec1f754d52c0c4e0c5542e2d56adb7
Daniel Silva Nunes <dsnunes@usp.br>
2020-09-28 22:18:08 -0300

    first version of algorithm that seems to work well

--- ep1/ep1.c
+++ ep1/ep1.c
@@ -21,23 +21,26 @@ struct Process {
   int tf;
   int tr;
   int exec_time;
+  pthread_t thread;
+  pthread_mutex_t interrupt;
 };
-// linked list of processes from tracefile
+
+// linked list of processes
 struct ProcessList {
   struct Process *process;
   struct ProcessList *next;
 };
 
+// structure containing simulation data
 struct Simulation {
   int seconds_elapsed;
   int context_switches;
 };
 
+pthread_mutex_t clock_mutex;
+
 // global simulator clock
 struct Simulation* simulation;
-
-// Mutexes
-pthread_mutex_t interrupt_process_mutex;
 
 int interrupt_running_process = 0;
 
@@ -78,23 +81,60 @@ void list_push(struct ProcessList **head, struct Process* process) {
   *head = new_head;
 }
 
-void *process_preemptive_work(void *args) {
-  struct Process* process = (struct Process*) args;
-  int exec_time = 0;
-  while (process->dt - process->exec_time != 0) {
-    sleep(1);
-    simulation->seconds_elapsed++;
-    process->exec_time++;
-    if (interrupt_running_process) {
-      pthread_mutex_lock(&interrupt_process_mutex);
-      interrupt_running_process = 0;
-      pthread_mutex_unlock(&interrupt_process_mutex);
-      break;
+void list_insert_by_time_left(struct ProcessList **head, struct Process* process) {
+  struct ProcessList* list_pointer = *head;
+  struct ProcessList* new_proc = malloc(sizeof(struct ProcessList*));
+  new_proc->process = process;
+  if (list_pointer == NULL || list_pointer->process->dt >= new_proc->process->dt) {
+    new_proc->next = *head;
+    *head = new_proc;
+  } else {
+    list_pointer = *head;
+    while (list_pointer->next != NULL && list_pointer->process->dt > new_proc->process->dt) {
+      list_pointer = list_pointer->next;
     }
+    new_proc->next = list_pointer->next;
+    list_pointer->next = new_proc;
   }
 }
 
-void *process_work(void *args) {
+// Removes and returns the first Process of a processlist
+struct Process* list_pop(struct ProcessList **head) {
+  if (*head == NULL)
+    return NULL;
+  // get the first element
+  struct ProcessList* first_element = *head;
+  struct Process* p = first_element->process;
+  // makes first element the next of the first
+  *head = (*head)->next;
+  // free head ref
+  free(first_element);
+  return p;
+}
+
+void *preemptive_worker(void *args) {
+  struct Process* process = (struct Process*) args;
+  // process executes until the end
+  printf("called thread for process %s\n", process->name);
+  while((process->dt - process->exec_time) != 0) {
+    // but it can be interrupted
+    
+    pthread_mutex_lock(&(process->interrupt));
+    printf("running %s time: %d s\n", process->name, simulation->seconds_elapsed);
+    pthread_mutex_lock(&clock_mutex);
+    process->exec_time++;
+    simulation->seconds_elapsed++;
+    pthread_mutex_unlock(&clock_mutex);
+    pthread_mutex_unlock(&(process->interrupt));
+    sleep(1);
+    process->tf = simulation->seconds_elapsed;
+    process->tr = process->tf - process->arrival_time;
+  }
+  printf("%s encerrou seu trabalho\n", process->name);
+  return NULL;
+}
+
+void *worker(void *args) {
   struct Process* process = (struct Process*) args;
   int dt = process->dt;
   int exec_time = 0;
@@ -116,10 +156,10 @@ void fcfs(struct ProcessList* incoming) {
     if (process->arrival_time <= simulation->seconds_elapsed) {
       // execute current process until it finishes
       simulation->context_switches++;
-      pthread_t process_thread;
-      pthread_create(&process_thread, NULL, process_work, process);
-      pthread_join(process_thread, NULL);
-      // move forward on process trace
+      pthread_create(&(process->thread), NULL, worker, process);
+      // wait current process to finish
+      pthread_join(process->thread, NULL);
+      // then move forward on process trace
       incoming = incoming->next;
     } else {
       // keep waiting for a new process
@@ -134,42 +174,72 @@ int time_left(struct Process* p) {
 }
 
 int process_finished(struct Process* p) {
-  return p->exec_time == p->dt;
+  return p == NULL || p->exec_time == p->dt;
 }
 
 void srtn(struct ProcessList* incoming) {
-  struct ProcessList* ready = malloc(sizeof(struct ProcessList*));
+  struct ProcessList* ready = NULL;
+  // scheduler clock
+  int local_time = -1;
+  // currently running process
   struct Process* running = NULL;
-  pthread_mutex_init(&interrupt_process_mutex, NULL);
-  pthread_t running_thread;
-  // schedule while there are process left to run
-  while (incoming != NULL) {
-    // printf(">> %s %d s\n", running->name, simulation->seconds_elapsed);
-    // next process has arrived
-    if (incoming->process->arrival_time == simulation->seconds_elapsed) {
-      struct Process* next_proc = incoming->process;
-      // if a process is already running
-      if (running != NULL) {
-        // check if current process will replace the active process
-        if (next_proc->dt < (running->dt - running->exec_time)) {
-          pthread_mutex_lock(&interrupt_process_mutex);
-          interrupt_running_process = 1;
-          pthread_mutex_unlock(&interrupt_process_mutex);
-          pthread_create(&running_thread, NULL, process_preemptive_work, next_proc);
-          running = next_proc;
-        } 
+  while (incoming != NULL || ready != NULL) {
+    pthread_mutex_lock(&clock_mutex);
+    local_time = simulation->seconds_elapsed;
+    // if a new process has arrived
+    if (incoming && local_time == incoming->process->arrival_time) {
+      printf("Chegou %s em %d\n", incoming->process->name, local_time);
+      if (running == NULL) {
+        pthread_create(&(incoming->process->thread), NULL, preemptive_worker, incoming->process);
+        running = incoming->process;
+        simulation->context_switches++;
       } else {
-        pthread_create(&running_thread, NULL, process_preemptive_work, next_proc);
-        running = next_proc;
+        // interrupt currently running process
+        pthread_mutex_lock(&(running->interrupt));
+        printf("Interrompeu processo %s rodando tempo restante: %d para verificar\n", running->name, time_left(running));
+        // if new process has shorter dt, keep running process locked
+        // and starts the new one. Place older process on ready list
+        if (incoming->process->dt < time_left(running)) {
+          printf("Deixa %s interrompido por enquanto\n", running->name);
+          list_insert_by_time_left(&ready, running);
+          running = incoming->process;
+          pthread_create(&(incoming->process->thread), NULL, preemptive_worker, running);
+          simulation->context_switches++;
+        } else {
+          printf("Libera %s de novo pois ele pode continuar rodando\n", running->name);
+          pthread_mutex_unlock(&(running->interrupt));
+          printf("Guarda %s interrompido na fila\n", incoming->process->name);
+          pthread_mutex_lock(&(incoming->process->interrupt));
+          list_insert_by_time_left(&ready, incoming->process);
+        }
       }
-      // once next process is handled, move forward on incoming list
       incoming = incoming->next;
-    } else if (running == NULL) {
-      sleep(1);
-      simulation->seconds_elapsed++;
+      // if no process has arrived but currently running process has finished
+    } else if (process_finished(running)) {
+      pthread_mutex_unlock(&(running->interrupt));
+      if (ready != NULL) {
+        struct Process* next_ready = list_pop(&ready);
+        printf("Got %s from ready\n", next_ready->name);
+        simulation->context_switches++;
+        pthread_mutex_unlock(&(next_ready->interrupt));
+        running = next_ready;
+        // only creates a new thread if process has never run
+        if (next_ready->exec_time == 0)
+          pthread_create(&(next_ready->thread), NULL, preemptive_worker, next_ready);
+      } else {
+        // idle state, when a process has finished and no process is waiting,
+        // but new processes still on their way
+        printf("idle state\n");
+        sleep(1);
+        simulation->seconds_elapsed++;
+      }
     }
+    pthread_mutex_unlock(&clock_mutex);
   }
-  pthread_join(running_thread, NULL);
+  if (running != NULL) {
+    printf("Waiting remaining thread...\n");
+    pthread_join(running->thread, NULL);
+  }
 }
 
 void print_output_file(struct ProcessList* trace) {
@@ -188,6 +258,7 @@ void simulate(struct ProcessList* trace) {
   simulation = malloc(sizeof(struct Simulation*));
   simulation->seconds_elapsed = 0;
   simulation->context_switches = 0;
+  pthread_mutex_init(&clock_mutex, NULL);
   if (scheduler == 1) {
     fcfs(trace);
   } else if (scheduler == 2) {
@@ -215,6 +286,11 @@ struct Process* current_process(char* line) {
   process->dt = atoi(process_data[2]);
   process->deadline = atoi(process_data[3]);
   process->exec_time = 0;
+  pthread_t process_thread;
+  pthread_mutex_t interrupt;
+  pthread_mutex_init(&interrupt, NULL);
+  process->thread = process_thread;
+  process->interrupt = interrupt;
   return process;
 }
 
