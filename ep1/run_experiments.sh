298494f3ae54a94e41d45209b539adab63bc2d23
Daniel Silva Nunes <daniel.silva@enjoei.com.br>
2020-10-01 17:57:17 -0300

    script to run experiments

---
+++ ep1/run_experiments.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+REPEATS=30
+N_PROCS=(10 100 1000)
+
+# for each scheduler...
+# 2 SCHEDULERS ALREADY IMPLEMENTED
+for ((SCHEDULER=1; SCHEDULER <= 2; SCHEDULER++ )); do
+  echo "Escalonador: $SCHEDULER"
+  # for each amount of processes
+  for N in ${N_PROCS[@]}; do
+    echo "Quantidade de processos: $N"
+    for ((R=1; R <= REPEATS; R++)); do
+      ./ep1 ${SCHEDULER} analysis/data/analysis_trace${N_PROCS} analysis/results/s_${SCHEDULER}_p_${N_PROCS}_n_${R}_out -d 2> analysis/logs/stderr_s_${SCHEDULER}_p_${N_PROCS}_n_${R}
+    done
+  done
+done
