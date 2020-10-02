5ae4460b36a010e660db8d783baea43b239ef629
Daniel Silva Nunes <dsnunes@usp.br>
2020-10-01 21:00:16 -0300

    fix test script

--- ep1/run_experiments.sh
+++ ep1/run_experiments.sh
@@ -11,7 +11,8 @@ for ((SCHEDULER=1; SCHEDULER <= 2; SCHEDULER++ )); do
   for N in ${N_PROCS[@]}; do
     echo "Quantidade de processos: $N"
     for ((R=1; R <= REPEATS; R++)); do
-      ./ep1 ${SCHEDULER} analysis/data/analysis_trace${N_PROCS} analysis/results/s_${SCHEDULER}_p_${N_PROCS}_n_${R}_out -d 2> analysis/logs/stderr_s_${SCHEDULER}_p_${N_PROCS}_n_${R}
+      # ./ep1 ${SCHEDULER} analysis/data/analysis_trace${N} analysis/results/s_${SCHEDULER}_p_${N}_n_${R}_out -d 2> analysis/logs/stderr_s_${SCHEDULER}_p_${N}_n_${R}
+      ./ep1 ${SCHEDULER} analysis/data/trace-test analysis/results/s_${SCHEDULER}_p_${N}_n_${R}_out -d 2> analysis/logs/stderr_s_${SCHEDULER}_p_${N}_n_${R}
     done
   done
 done
