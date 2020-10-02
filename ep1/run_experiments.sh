b3875fb86db12e4fbec46ca3c824b2d4ddc01344
Daniel Silva Nunes <dsnunes@usp.br>
2020-10-02 17:55:38 -0300

    another script

--- ep1/run_experiments.sh
+++ ep1/run_experiments.sh
@@ -8,5 +8,5 @@ N_PROCS=$2
 echo "run_experiments.sh: escalonador: $SCHEDULER numero de processos: $N_PROCS"
 
 for ((R=1; R <= REPEATS; R++)); do
-  ./ep1 ${SCHEDULER} analysis/data/analysis_trace${N_PROCS}_${R} analysis/results/s_${SCHEDULER}_p_${N_PROCS}_out -d 2> analysis/logs/stderr_s_${SCHEDULER}_p_${N_PROCS}_n_${R}
+  ./ep1 ${SCHEDULER} analysis/data/analysis_trace${N_PROCS} analysis/results/s_${SCHEDULER}_p_${N_PROCS}_out -d 2> analysis/logs/stderr_s_${SCHEDULER}_p_${N_PROCS}_n_${R}
 done
