8d2b88ff18cc0341e4198357f50f0fd2baf2d436
Daniel Silva Nunes <dsnunes@usp.br>
2020-11-24 20:58:23 -0300

    regex para encontrar entradas de arquivos e diretórios

--- ep3/patterns.py
+++ ep3/patterns.py
@@ -5,4 +5,4 @@ FILE_OBJ = r'([\^][\w.]{1,16}&[a-f0-9A-F]{7}(?:(?:[&][a-fA-F0-9]+){4}))'
 # Captura a string representando diretório
 DIR_OBJ = r'([\%][\w.]{1,16}(?:(?:[&][0-9]+){4}))'
 
-ENTRY_BY_NAME = r'([\^]\b(name)\b&[a-f0-9A-F]{7}(?:(?:[&][a-fA-F0-9]+){4})\|)'
+ENTRY_BY_NAME = r'([\^|\%]\b(name)\b[&]{0,1}[a-f0-9A-F]{0,7}(?:(?:[&][a-f0-9]+){4})\|)'
